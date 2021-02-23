8f7e421921fe68e3c1cf8783aeeeeaa6b5cfc05b
Helis Montes <helismontes@gmail.com>
2021-02-22 23:30:52 -0500

    Clases
    Herencias, get y set

--- clases.js
+++ clases.js
@@ -1,14 +1,60 @@
 "use strict";
-class Avengers {
-    constructor(name, team, nameR, puedePelear, peleasGanadas) {
-        this.puedePelear = false;
-        this.peleasGanadas = 0;
+class Humano {
+    constructor(name, lastname, sex, sentidos) {
         this.nombre = name;
-        this.equipo = team;
-        this.nombreReal = nameR;
-        this.puedePelear = puedePelear;
-        this.peleasGanadas = peleasGanadas;
+        this.apellido = lastname;
+        this.sexo = sex;
+        this.sentidos = sentidos;
+    }
+    get getNombre() {
+        return this.nombre;
+    }
+    get getApellido() {
+        return this.apellido;
+    }
+    get getSexo() {
+        return this.sexo;
+    }
+    get getSentidos() {
+        return this.sentidos;
+    }
+    set setNombre(name) {
+        this.nombre = name;
+    }
+    caminar() {
+        return `Si se caminar`;
+    }
+    consultarBD() {
+        const bd = {
+            nombre: "Josefina",
+            apellido: "Gutierrez",
+            sexo: "F",
+            sentidos: ["Gusto", "Tacto", "Oido", "Olfato"]
+        };
+        this.nombre = bd.nombre;
+        this.apellido = bd.apellido;
+        this.sexo = bd.sexo;
+        this.sentidos = bd.sentidos;
     }
 }
-let atman = new Avengers("Atman", "CAP", "Scott Lang", true, 2);
-console.log(atman);
+class Animal extends Humano {
+    constructor(tipo, genero, raza, name, lastname, sex, sentidos) {
+        super(name, lastname, sex, sentidos);
+        this.tipo = tipo;
+        this.genero = genero;
+        this.raza = raza;
+    }
+    caminar() {
+        return super.caminar();
+    }
+}
+const humano = new Humano("Helis", "Montes", "Masculino", ["Vista", "Gusto", "Tacto", "Oido", "Olfato"]);
+console.log(`clase Humano -`, humano);
+const perro = new Animal("Perro", "Canis", ["Pastor Aleman", "Lobo siberiano"], "Rog", "", "Macho", ["Gusto", "Tacto", "Oido", "Olfato"]);
+console.log(`clase Animal -`, perro);
+console.log(perro.caminar());
+console.log(humano.getNombre);
+console.log(humano.setNombre = "Jose");
+console.log(humano.getNombre);
+humano.consultarBD();
+console.log(humano.getNombre, humano.getApellido, humano.getSexo, humano.getSentidos);
--- clases.ts
+++ clases.ts
@@ -1,20 +1,78 @@
-class Avengers {
-    nombre:string;
-    equipo:string;
-    nombreReal:string;
+class Humano{
+    public nombre:string;
+    public apellido:string;
+    public sexo:string;
+    public sentidos:string[];
 
-    puedePelear?:boolean = false;
-    peleasGanadas?:number = 0;
-
-    constructor(name:string, team:string, nameR:string, puedePelear?:boolean, peleasGanadas?:number){
+    constructor(name:string, lastname:string, sex:string, sentidos:string[]){
         this.nombre = name;
-        this.equipo = team;
-        this.nombreReal = nameR;
-        this.puedePelear = puedePelear;
-        this.peleasGanadas = peleasGanadas;
+        this.apellido = lastname;
+        this.sexo = sex;
+        this.sentidos = sentidos;
+    }
+
+    get getNombre():string{
+        return this.nombre;
+    }
+    get getApellido():string{
+        return this.apellido;
+    }
+    get getSexo():string{
+        return this.sexo;
+    }
+    get getSentidos():string[]{
+        return this.sentidos;
+    }
+    set setNombre(name:string){
+        this.nombre = name;
+    }
+
+    protected caminar():string{
+        return `Si se caminar`
+    }
+
+    public consultarBD():void{
+        const bd:{nombre:string, apellido:string, sexo:string, sentidos:string[]} = {
+            nombre: "Josefina",
+            apellido: "Gutierrez",
+            sexo: "F",
+            sentidos: ["Gusto", "Tacto", "Oido", "Olfato"]
+
+        }
+        
+        this.nombre = bd.nombre;
+        this.apellido = bd.apellido;
+        this.sexo = bd.sexo;
+        this.sentidos = bd.sentidos;
+    }
+
+}
+
+class Animal extends Humano{
+    public tipo:String;
+    public genero:String;
+    public raza:string[]; 
+
+    constructor(tipo:String, genero:String, raza:string[],name:string, lastname:string, sex:string, sentidos:string[]){
+        super(name, lastname, sex, sentidos)
+        this.tipo = tipo;
+        this.genero = genero;
+        this.raza = raza;
+    }
+
+    public caminar():string{
+        return super.caminar();
     }
 }
 
-let atman:Avengers = new Avengers("Atman", "CAP", "Scott Lang", true, 2 );
+const humano:Humano = new Humano("Helis", "Montes", "Masculino", ["Vista", "Gusto", "Tacto", "Oido", "Olfato"])
+console.log(`clase Humano -`, humano)
 
-console.log(atman)
+const perro:Animal = new Animal("Perro", "Canis", ["Pastor Aleman", "Lobo siberiano"], "Rog", "", "Macho",["Gusto", "Tacto", "Oido", "Olfato"])
+console.log(`clase Animal -`, perro)
+
+
+console.log(perro.caminar());
+
+humano.consultarBD()
+console.log(humano.getNombre, humano.getApellido, humano.getSexo, humano.getSentidos)
